78eac5ce71d38a947326c95f510512cce7f6dcfb
Vishal Kumar Manchala <vishalxk@protonmail.com>
2023-09-05 21:29:07 +0530

    Change text

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-import { styler, spring, listen, pointer, value } from 'popmotion'
+// import { styler, spring, listen, pointer, value } from 'popmotion'
+
+// const me = document.querySelector("#memoji");
+// const imgStyler = styler(me);
+// const imgXY = value({ x: 0, y: 0 }, imgStyler.set);
+
+// listen(me, "mousedown touchstart").start(e => {
+//   e.preventDefault();
+//   pointer(imgXY.get()).start(imgXY);
+// });
+
+
+// listen(document, "mouseup touchend").start(() => {
+//   spring({
+//     from: imgXY.get(),
+//     velocity: imgXY.getVelocity(),
+//     to: { x: 0, y: 0 },
+//     stiffness: 200
+//     // mass: 1,
+//     // damping: 10
+//   }).start(imgXY);
+// });
+
+import {animate, spring} from 'motion'
 
 const me = document.querySelector("#memoji");
-const imgStyler = styler(me);
-const imgXY = value({ x: 0, y: 0 }, imgStyler.set);
 
-listen(me, "mousedown touchstart").start(e => {
-  e.preventDefault();
-  pointer(imgXY.get()).start(imgXY);
-});
-
-
-listen(document, "mouseup touchend").start(() => {
-  spring({
-    from: imgXY.get(),
-    velocity: imgXY.getVelocity(),
-    to: { x: 0, y: 0 },
-    stiffness: 200
-    // mass: 1,
-    // damping: 10
-  }).start(imgXY);
-});
+me.addEventListener('dragstart', (event) => {
+  console.log(event)
+})
